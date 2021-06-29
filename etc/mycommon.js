8fd07a91edd0afb79a4fbf018d9aa6f8fb239a0c
Jeongseup <thswjdtmq4@inu.ac.kr>
2021-06-29 11:23:09 +0900

    [Day24] 자바스크립트 고급 문법 메모

--- etc/mycommon.js
+++ etc/mycommon.js
@@ -1,3 +1,9 @@
+/**
+ * description : 최소값과 최대값을 파라미터로 받고 그 사이의 무작위(random) 정수를 반환하는 함수
+ * @param {Number} min 
+ * @param {Number} max 
+ * @returns {Number} min과 max 사이의 랜덤 정수 값
+ */
 function getRandomInteger(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
