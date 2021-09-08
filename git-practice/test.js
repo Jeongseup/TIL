3a88a2db852df2f396ea614f16404597d1c9a1ba
Jeongseup <thswjdtmq4@inu.ac.kr>
2021-09-08 23:43:08 +0900

    [ADD] git practice file upload

---
+++ git-practice/test.js
@@ -0,0 +1,11 @@
+console.log("diff");
+
+var list = [];
+console.log(list);
+
+for (var i = 0; i < 10; i++) {
+    // console.log(i);
+    list[i] = i;
+}
+
+console.log(list);
---
+++ git-practice/test.py
@@ -0,0 +1,14 @@
+print("code runner")
+
+test = list();
+# print(test);
+
+for i in range(0, 10):
+    # print(i)
+    test.append(i)
+
+print(test)
+
+
+test2 = [i for i in range(0, 10 )]
+print(test2);
