eb5d41ee677ea402f055e28daaf77f15b0b03413
Jeongseup <thswjdtmq4@inu.ac.kr>
2021-09-14 09:53:53 +0900

    [TIL] Js & HTML debug practice

---
+++ web/makeup-node.js/test.js
@@ -0,0 +1,20 @@
+let st = 3;
+// let st : number
+
+class Student {
+    constructor(_name, _korean, _english) {
+        this.name = _name;
+        this.korean = _korean;
+        this.english = _english;
+    }
+
+    getSum() {
+        return this.english + this.english;
+    }
+
+    getSum() {
+        return this.english + this.english;
+    }
+}
+
+let instance = new Student();
