30a841941197780b0ffbc8b4f68a56981b33e0e9
Jeongseup <thswjdtmq4@inu.ac.kr>
2021-10-02 23:21:20 +0900

    Jade view engine

--- 생활코딩/server_side_javascript/app.js
+++ 생활코딩/server_side_javascript/app.js
@@ -1,8 +1,42 @@
 const express = require("express");
 const app = express();
 
+app.locals.pretty = true;
+app.set("view engine", "jade");
+app.set("views", "./views");
+app.use(express.static("public"));
+
+
+
+
+app.get("/template", (req, res) => {
+    res.render("temp", { time: Date(), _title: "Jade" });
+});
+
 app.get("/", function (req, res) {
     res.send("hello js");
+});
+
+app.get("/dynamic", (req, res) => {
+    var output = `
+    <!DOCTYPE html>
+    <html lang="en">
+        <head>
+            <meta charset="UTF-8" />
+            <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+            <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+            <title>Document</title>
+        </head>
+        <body>
+            Hello dynamic
+        </body>
+    </html>
+    `;
+    res.send(output);
+});
+
+app.get("/route", (req, res) => {
+    res.send("route!");
 });
 
 app.get("/login", (req, res) => {
