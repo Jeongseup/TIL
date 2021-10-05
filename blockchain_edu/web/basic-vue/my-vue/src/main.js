daf2a47ae696fbf6e9b121000ca17266b27233c6
Jeongseup <thswjdtmq4@inu.ac.kr>
2021-10-05 23:36:25 +0900

    vue공부 & 서버사이드 자크스립트 -> nodejs

---
+++ blockchain_edu/web/basic-vue/my-vue/src/main.js
@@ -0,0 +1,11 @@
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
+
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap";
+
+const app = createApp(App);
+
+app.use(store).use(router).mount("#app");
