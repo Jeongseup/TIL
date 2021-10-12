74a10ab17121faf0f16692bf36661d0428a2e7ae
Jeongseup <thswjdtmq4@inu.ac.kr>
2021-10-12 23:58:25 +0900

    plus pollWeb3

--- vue/betting-dapp/src/network/getWeb3.js
+++ vue/betting-dapp/src/network/getWeb3.js
@@ -4,13 +4,21 @@ const getWeb3 = async () => {
     let result = {}
 
     const web3 = new Web3(Web3.givenProvider)
+    console.log(web3.currentProvider)
+    console.log(web3.eth.currentProvider)
 
     var isInjected = await web3.eth.net.isListening()
     var coinbase = await web3.eth.getCoinbase()
     var balance = await web3.eth.getBalance(coinbase)
     var chainId = await web3.eth.net.getId()
 
-    result = { ...result, isInjected, coinbase, balance, chainId }
+    result = {
+        ...result,
+        isInjected,
+        coinbase,
+        balance,
+        chainId
+    }
 
     return result
 }
---
+++ vue/betting-dapp/src/network/pollWeb3.js
@@ -0,0 +1,11 @@
+// import Web3 from 'web3'
+// import { state } from '../store'
+
+// const pollWeb3 = function(state) {
+//     let web3 = window.web3
+//     web3 = new Web3(web3.currentProvider)
+
+//     setInterval(() => {}, 700)
+// }
+
+// export default pollWeb3
